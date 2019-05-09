cd9565963f7d137b2a2d9f143e5e9331f9403177
Wilmer Llamas <wllamasr@gmail.com>
2019-05-09 09:50:13 -0500

    feat: Validation when creating an schedule

--- app/Controllers/SchedulesController.php
+++ app/Controllers/SchedulesController.php
@@ -17,32 +17,35 @@ class SchedulesController
 
     public function store(Request $request)
     {
-        /*
-        insert into `horarios` (hora_inicio, hora_fin, asignaturas_pensums_id, instalaciones_id)
-        values (?);
-        */
-
+        /**
+         * select * from `horarios`
+         * where (`hora_inicio` <= ? and `hora_fin` >= ?) 
+         * or (`hora_inicio` <= ? and `hora_fin` >= ?) 
+         * and (`dia_semana` = ? and `instalaciones_id` = ?)
+         */
         $exist_horario = DB::table('horarios')
             ->where(function ($query) use ($request) {
-                $query->where([
-                    ['hora_inicio', '>', $request->hora_inicio],
-                    ['hora_fin', '<', $request->hora_inicio],
-                ])->orWhere([
-                    ['hora_inicio', '>', $request->hora_fin],
-                    ['hora_fin', '<', $request->hora_fin],
-
-                ]);
+                $query->where('hora_inicio', '<=', $request->hora_inicio)
+                    ->where('hora_fin', '>=', $request->hora_inicio);
+            })
+            ->orWhere(function ($query) use ($request) {
+                $query->where('hora_inicio', '<=', $request->hora_final)
+                    ->where('hora_fin', '>=', $request->hora_final);
             })
             ->where([
                 ['dia_semana', $request->dia_semana],
                 ['instalaciones_id', $request->instalacion]
             ])
-            ->get();
-
-        if (!$exist_horario) {
+            ->toSql();
+        return $exist_horario;
+        if ($exist_horario->count() == 0) {
+            /*
+                insert into `horarios` (hora_inicio, hora_fin, asignaturas_pensums_id, instalaciones_id)
+                values (?);
+                */
             DB::table('horarios')->insert([
                 'hora_inicio' => $request->hora_inicio,
-                'hora_fin' => $request->hora_fin,
+                'hora_fin' => $request->hora_final,
                 'asignaturas_pensums_id' => $request->asignatura,
                 'instalaciones_id' => $request->instalacion,
                 'dia_semana' => $request->dia_semana
